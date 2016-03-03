5b4ac9b66221cc80386ac3c3d4558dabfbb5a03e
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-03-03 19:30:26 +0200

    Simplified

--- src/bmi-control.js
+++ src/bmi-control.js
@@ -14,7 +14,7 @@ const BMI = ({bmi}) =>
   <K.div {...classes("bmi", K(bmi, M.BMI.classification))}>
     <Slider title="Weight" units="kg" min={40}  max={140} value={bmi.lens(M.BMI.weight)}/>
     <Slider title="Height" units="cm" min={140} max={210} value={bmi.lens(M.BMI.height)}/>
-    <K.div>BMI: <K.span className="bmi-value">{K(bmi, M.BMI.bmi)}</K.span></K.div>
+    <div>BMI: <K.span className="bmi-value">{K(bmi, M.BMI.bmi)}</K.span></div>
   </K.div>
 
 export default ({bmi = Atom(M.mock)} = {}) => <BMI bmi={bmi.lens(M.BMI.augment)}/>
