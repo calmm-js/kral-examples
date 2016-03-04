8446ecfff477324148ada3dd4ba11ed76897481a
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-03-04 19:10:16 +0200

    Clarify

--- src/clock.js
+++ src/clock.js
@@ -2,6 +2,9 @@ import K     from "kefir-react-html"
 import Kefir from "kefir"
 import React from "react"
 
-const seconds = Kefir.constant().merge(Kefir.interval(1000))
+const oncePerSecond = Kefir.constant().merge(Kefir.interval(1000))
 
-export default () => <K.div>{K(seconds, () => new Date().toString())}</K.div>
+export default () =>
+  <K.div>
+    {K(oncePerSecond, () => new Date().toString())}
+  </K.div>
