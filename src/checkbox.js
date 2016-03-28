2f3b8315c1e99aa963e1f8f977f1c75c73556a31
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-03-28 16:14:11 +0300

    Updated

--- src/checkbox.js
+++
@@ -1,9 +0,0 @@
-import Atom      from "kefir.atom"
-import K, {bind} from "kefir.react.html"
-import React     from "react"
-
-export default ({checked = Atom(false)}) =>
-  <div>
-    <label><K.input type="checkbox" {...bind({checked})}/>Toggle me</label>
-    <K.p>{K(checked, c => c ? "ON" : "off")}</K.p>
-  </div>
--- src/converter.js
+++
@@ -1,6 +0,0 @@
-import Atom      from "kefir.atom"
-import K, {bind} from "kefir.react.html"
-import React     from "react"
-
-export default ({value = Atom("0")}) =>
-  <K.p><K.input {...bind({value})}/>°C is {K(value, c => c * 9/5 + 32)}°F</K.p>
