2da1afc9184346fb6a7681cfcd928e141202c9ac
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-05-06 07:43:21 +0300

    Changed to work with latest React.

--- src/text-input.js
+++ src/text-input.js
@@ -1,23 +1,24 @@
-import Atom           from "kefir.atom"
-import K, {fromKefir} from "kefir.react.html"
-import React          from "react"
+import Atom  from "kefir.atom"
+import K     from "kefir.react.html"
+import React from "react"
 
 export default ({value = Atom("")}) => {
   const editing = Atom(false)
   const exit = () => editing.set(false)
   const save = e => {value.set(e.target.value); exit()}
-  return fromKefir(K(editing, e => e
-    ? <K.input key="1"
-               type="text"
-               autoFocus
-               onFocus={({target: t}) => t.selectionStart = t.value.length}
-               defaultValue={value}
-               onKeyDown={e => e.key === "Enter"  && save(e)
-                            || e.key === "Escape" && exit()}
-               onBlur={save}/>
-    : <K.input key="0"
-               type="text"
-               disabled
-               {...{value}}
-               onDoubleClick={() => editing.set(true)}/>))
+  return <K.span onDoubleClick={() => editing.set(true)}>
+      {K(editing, e =>
+         e ? <K.input key="1"
+                      type="text"
+                      autoFocus
+                      onFocus={({target: t}) => t.selectionStart = t.value.length}
+                      defaultValue={value}
+                      onKeyDown={e => e.key === "Enter"  && save(e)
+                                   || e.key === "Escape" && exit()}
+                      onBlur={save}/>
+           : <K.input key="2"
+                      type="text"
+                      disabled
+                      {...{value}}/>)}
+    </K.span>
 }
